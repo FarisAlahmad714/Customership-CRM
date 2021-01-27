876e6f6b7151973f04ee6a8a31e77128013096b3
FarisAlahmad714 <Farisalahmad01@Hotmail.com>
2021-01-26 16:02:49 -0800

    Set url and view paths building blocks

--- Leads/models.py
+++ Leads/models.py
@@ -8,8 +8,12 @@ class User(AbstractUser):
     pass
 
 class Agent(models.Model):
-    user = models.OneToOneField(User , on_delete=models.CASCADE)
-
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    # returns string representation of the model
+    def __str__(self):
+        return self.user.email
+    
 class Lead(models.Model):
     
     # SOURCE_CHOICES = (
@@ -24,6 +28,9 @@ class Lead(models.Model):
     agent = models.ForeignKey("Agent" , on_delete=models.CASCADE)
     # ONCASCADE IS GOING TO DELETE ALL LEADS UNDER AGENT DELETED, MOVE LEADS TO ANOTHER AGENT BEFORE DELETTION
     
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
     # contacted = models.BooleanField(default=False)
     # source = models.CharField(choices=SOURCE_CHOICES, max_lenght = 100)
     # profile_pic = models.ImageField(blank=True, null=True)
--- Leads/views.py
+++ Leads/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Lead
 
 # Create your views here.
+
+def home(request):
+    
+    leads = Lead.objects.all()
+    
+    # return HttpResponse("YO")
+    context = {
+        "leads":leads
+    }
+    return render(request, "home.html", context)
